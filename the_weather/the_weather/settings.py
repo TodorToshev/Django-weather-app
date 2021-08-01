c0ab2cb6f89fee5d8e8c70df0d2a13210ec9bb49
Todor Toshev <t.toshev95@gmail.com>
2021-08-01 14:42:29 +0300

    Added main function, html + css templates;  edited HTML+CSS, extended main function, changed layout of the weather info cards, added form in forms.py for custom form input field and a 'City' model for registering cities in the DB.

--- the_weather/the_weather/settings.py
+++ the_weather/the_weather/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJ-KEY")
+SECRET_KEY = '***YOUR_SECRET_KEY***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #custom
+    "weather",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#CUSTOM:
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL ="/media/"
--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from django.shortcuts import render, redirect
+from .models import City
+from .forms import CityForm
+from django.contrib import messages
+import urllib.request
+import json
+
+import os
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            new_city = form.cleaned_data["name"]
+            start_url = ("http://api.openweathermap.org/data/2.5/weather?q=" + 
+                        new_city + f"&units=metric&appid={os.environ.get('WEATHER-API-KEY')}")
+            url = start_url.replace(" ", "+")
+
+            # avoids 404 code from weather provider:
+            try:
+                source = urllib.request.urlopen(url).read()
+                list_of_data = json.loads(source)
+            except:
+                context = {"form": form, "message": messages.error(request, "City not found.")}
+                return render(request, "weather/weather.html", context)
+
+            # checks if the city is already in the DB:
+            existing_city_count = City.objects.filter(name=new_city).count()
+            if existing_city_count == 0:
+                form.save()
+            else:
+                messages.warning(request, "City already exists.")
+
+    form = CityForm
+    cities = City.objects.all()
+    weather_data = []
+
+    for city in cities:
+        data = {
+            "city": city.name,
+            "country_code":str(list_of_data["sys"]["country"]),
+            "coordinate":str(list_of_data["coord"]["lon"]) + ", " + 
+                str(list_of_data["coord"]["lat"]),
+            "temp":str(list_of_data["main"]["temp"]) + " C",
+            "pressure":str(list_of_data["main"]["pressure"]),
+            "humidity":str(list_of_data["main"]["humidity"]),
+            "main":str(list_of_data["weather"][0]["main"]),
+            "description":str(list_of_data["weather"][0]["description"]),
+            "icon":str(list_of_data["weather"][0]["icon"]),
+        }
+        weather_data.append(data)
+
+    context = {"weather_data": weather_data, "form": form}
+    return render(request, "weather/weather.html", context)
